864f255669b3ab38219df7d78c0a9c5608b52162
Denys Vladymyrov <link_amd@yahoo.com>
2024-10-28 21:11:51 +0200

    created pages and installe shadcn.ui

---
+++ app/orders/page.tsx
@@ -0,0 +1,5 @@
+function OrdersPage() {
+  return <h1>OrdersPage</h1>;
+}
+
+export default OrdersPage;
---
+++ app/reviews/page.tsx
@@ -0,0 +1,5 @@
+function ReviewsPage() {
+  return <h1>ReviewsPage</h1>;
+}
+
+export default ReviewsPage;
