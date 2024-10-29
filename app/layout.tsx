8aa6ea9e1a1d66d9f36deda924bfa227778c9f87
Denys Vladymyrov <link_amd@yahoo.com>
2024-10-30 01:41:08 +0200

    added global provider and theme changer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Navbar from '@/components/navbar/Navbar';
+import Container from '@/components/global/Container';
+
+import Providers from './providers';
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,12 +29,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Providers>
+              <Navbar />
+              <Container className='py-20'>{children}</Container>
+          </Providers>
       </body>
-    </html>
+      </html>
   );
 }
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,12 @@
-function NavSearch() {
-  return <div>NavSearch</div>
-}
+import { Input } from '../ui/input';
 
+function NavSearch() {
+  return (
+      <Input
+          type='search'
+          placeholder='search product...'
+          className='max-w-xs dark:bg-muted '
+      />
+  );
+}
 export default NavSearch;
