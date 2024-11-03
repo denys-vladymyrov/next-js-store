df6160b6bd5d44ac2c666f1a456f6ae01291d70c
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-03 18:16:38 +0200

    added ProductsGrid, update Hero, added skeleton

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,6 +1,11 @@
-function FavoriteToggleButton() {
-  return <div>
-    FavoriteToggleButton
-  </div>
+import { FaHeart } from 'react-icons/fa';
+import { Button } from '@/components/ui/button';
+
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+      <FaHeart />
+    </Button>
+  );
 }
 export default FavoriteToggleButton;
