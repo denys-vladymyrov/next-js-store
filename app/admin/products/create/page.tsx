5c10207efabf8d08d338e49561d7f00f6ed9dd52
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-05 01:21:55 +0200

    added FormInput cmp

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,8 +1,8 @@
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import FormInput from "@/components/form/FormInput";
 
 import { faker } from '@faker-js/faker';
+
 
 const createProductAction = async (formData: FormData) => {
   'use server';
@@ -18,10 +18,7 @@ function CreateProductPage() {
         <h1 className='text-2xl font-semibold mb-8 capitalize'>create product</h1>
         <div className='border p-8 rounded-md'>
           <form action={createProductAction}>
-            <div className='mb-2'>
-              <Label htmlFor='name'>Product Name</Label>
-                <Input id='name' name='name' type='text' defaultValue={name} />
-            </div>
+           <FormInput name={name} type='text' label="product name" defaultValue={name} />
             <Button type='submit' size='lg'>
               Submit
             </Button>
