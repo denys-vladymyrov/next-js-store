45adf9f009c222136ea314ea9397c9ab21e3bb7e
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-05 00:27:33 +0200

    fixed eslint errors

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,6 @@
 import { LuUser2 } from 'react-icons/lu';
 import { currentUser } from '@clerk/nextjs/server';
+import Image from "next/image";
 
 async function UserIcon() {
   const user = await currentUser();
@@ -7,7 +8,14 @@ async function UserIcon() {
 
   if (profileImage)
     return (
-      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' alt="profileImage" />
+        <Image
+            src={profileImage}
+            alt="profileImage"
+            width={24}
+            height={24}
+            priority
+            className='w-6 h-6 rounded-full object-cover'
+        />
     );
 
   return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
