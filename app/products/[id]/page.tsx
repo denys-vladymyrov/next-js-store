69faaca6a54285a0542f340932a45c9f6e311bdc
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-10 00:12:46 +0200

    added "share" button

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
+import ShareButton from '@/components/single-product/ShareButton';
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
@@ -30,15 +31,18 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         <div>
           <div className='flex gap-x-8 items-center'>
             <h1 className='capitalize text-3xl font-bold'>{name}</h1>
-            <FavoriteToggleButton productId={params.id} />
+            <div className='flex items-center gap-x-2'>
+              <FavoriteToggleButton productId={params.id}/>
+              <ShareButton name={product.name} productId={params.id}/>
+            </div>
           </div>
-          <ProductRating productId={params.id} />
+          <ProductRating productId={params.id}/>
           <h4 className='text-xl mt-2'>{company}</h4>
           <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
             {dollarsAmount}
           </p>
           <p className='mt-6 leading-8 text-muted-foreground'>{description}</p>
-          <AddToCart productId={params.id} />
+          <AddToCart productId={params.id}/>
         </div>
       </div>
     </section>
