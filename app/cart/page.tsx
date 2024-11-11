8728b7f98e1023a30bac37d79b7a38acd921eb3a
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-12 01:21:54 +0200

    added third columnt width amount dropdown

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -9,24 +9,24 @@ async function CartPage() {
   const { userId } = auth();
   if (!userId) redirect('/');
 
-  const cart = await fetchOrCreateCart({ userId });
-  await updateCart(cart);
+  const previousCart = await fetchOrCreateCart({ userId });
+  const { cartItems, currentCart } = await updateCart(previousCart);
 
-  if (cart.numItemsInCart === 0) {
+  if (cartItems.length === 0) {
     return <SectionTitle text='Empty cart' />;
   }
   return (
-      <>
-        <SectionTitle text='Shopping Cart' />
-        <div className='mt-8 grid gap-4 lg:grid-cols-12'>
-          <div className='lg:col-span-8'>
-            <CartItemsList cartItems={cart.cartItems} />
-          </div>
-          <div className='lg:col-span-4 lg:pl-4'>
-            <CartTotals cart={cart} />
-          </div>
+    <>
+      <SectionTitle text='Shopping Cart' />
+      <div className='mt-8 grid gap-4 lg:grid-cols-12'>
+        <div className='lg:col-span-8'>
+          <CartItemsList cartItems={cartItems} />
         </div>
-      </>
+        <div className='lg:col-span-4 lg:pl-4'>
+          <CartTotals cart={currentCart} />
+        </div>
+      </div>
+    </>
   );
 }
 export default CartPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -453,6 +453,9 @@ export const updateCart = async (cart: Cart) => {
     include: {
       product: true, // Include the related product
     },
+    orderBy: {
+      createdAt: 'asc',
+    },
   });
 
   let numItemsInCart = 0;
@@ -466,17 +469,20 @@ export const updateCart = async (cart: Cart) => {
   const shipping = cartTotal ? cart.shipping : 0;
   const orderTotal = cartTotal + tax + shipping;
 
-  await db.cart.update({
+  const currentCart = await db.cart.update({
     where: {
       id: cart.id,
     },
+
     data: {
       numItemsInCart,
       cartTotal,
       tax,
       orderTotal,
     },
+    include: includeProductClause,
   });
+  return { currentCart, cartItems };
 };
 
 export const addToCartAction = async (prevState: any, formData: FormData) => {
@@ -527,7 +533,60 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
   redirect('/orders');
 };
 
-export const removeCartItemAction = async () => {};
+export const removeCartItemAction = async (prevState: any, formData: FormData) => {
+  const user = await getAuthUser();
 
-export const updateCartItemAction = async () => {};
+  try {
+    const cartItemId = formData.get('id') as string;
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+
+    debugger;
+
+    await db.cartItem.delete({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+    });
+
+    await updateCart(cart);
+    revalidatePath('/cart');
+    return { message: 'Item removed from cart' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const updateCartItemAction = async ({amount, cartItemId}: {
+  amount: number;
+  cartItemId: string;
+}) => {
+  const user = await getAuthUser();
+
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+
+    await db.cartItem.update({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+      data: {
+        amount,
+      },
+    });
+
+    await updateCart(cart);
+    revalidatePath('/cart');
+    return { message: 'cart updated' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 
