38692b7c352594872b6f10ffbf1d2a5615b0dee1
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-13 00:53:39 +0200

    added test login and pass to navbar

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -4,11 +4,17 @@ import { SiWebstorm } from "react-icons/si";
 
 function Logo() {
   return (
+    <>
       <Button size='icon' asChild>
         <Link href='/'>
           <SiWebstorm className='w-6 h-6' />
         </Link>
       </Button>
+      <div className="flex flex-col">
+        <div className="text-sm">Test admin login: <span className="font-bold">link_amd@yahoo.com</span></div>
+        <div className="text-sm">Password: <span className="font-bold">admin1245!</span></div>
+      </div>
+    </>
   );
 }
 
